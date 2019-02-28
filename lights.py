1ebf65630b22912004f029c029f7ce93627b0f37
Ryan Byrne <ryanbyrne142@yahoo.com>
2019-02-28 14:42:33 -0500

    add pixel simulation capability

--- lights.py
+++ lights.py
@@ -1,16 +1,52 @@
+import sys
+
+status = {}
+
 class Lights():
 
     @staticmethod
     def control(on, off):
-        print "Turn on"
         for stop in on:
-            print stop
-        print "Turn off"
+            if stop[0] not in status:
+                status[stop[0]] = [stop[1]]
+            else:
+                if stop[1] not in status[stop[0]]:
+                    status[stop[0]].append(stop[1])
+                else:
+                    continue
         for stop in off:
-            print stop
+            if stop[0] not in status:
+                status[stop[0]] = []
+            else:
+                try:
+                    status[stop[0]].remove(stop[1])
+                except ValueError:
+                    print status[stop[0]]
+                    continue
+        # Here's where the magic happens baby. Later add LED control via
+        # the raspberry Pi.
+        for stop in status.keys():
+            if status[stop] == []:
+                signal = "off"
+            elif len(status[stop]) > 1:
+                signal = Lights.color_blend(status[stop])
+            else:
+                signal = status[stop][0]
+            print stop, signal
+
+    @staticmethod
+    def color_blend(color_array):
+        return color_array
+
+    @staticmethod
+    def test():
+        pass
+        #client.put_pixels(pixels, channel=0)
 
     @staticmethod
     def run(stops, old_stops):
+        client = Lights.TEST_CONNECT()
+        print client
         off = []
         on = []
         for stop in stops:
@@ -19,4 +55,4 @@ class Lights():
         for stop in old_stops:
             if stop not in stops:
                 off.append(stop)
-        Lights.control(on, off)
+        #Lights.control(on, off)
--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-import time
+import time, opc, sys
 from generate_stops import Generate
 from lights import Lights as l
+
+
+#-------------------------------------------------------------------------------
+# handle command line
+if len(sys.argv) == 1:
+    IP_PORT = '127.0.0.1:7890'
+elif len(sys.argv) == 2 and ':' in sys.argv[1] and not sys.argv[1].startswith('-'):
+    IP_PORT = sys.argv[1]
+else:
+    print('''
+Usage: raver_plaid.py [ip:port]
+
+If not set, ip:port defauls to 127.0.0.1:7890
+''')
+    sys.exit(0)
+#-------------------------------------------------------------------------------
+# connect to server
+client = opc.Client(IP_PORT)
+if client.can_connect():
+    print('    connected to %s' % IP_PORT)
+else:
+    # can't connect, but keep running in case the server appears later
+    print('    WARNING: could not connect to %s' % IP_PORT)
+print('')
+
+
+#-------------------------------------------------------------------------------
+# send pixels
+
+print('    sending pixels forever (control-c to exit)...')
+print('')
 
 if __name__ == '__main__':
     old_stops = []
