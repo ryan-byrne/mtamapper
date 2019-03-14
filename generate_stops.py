d9292c44d5f81850bdfc5979c577eb5db0eeacf7
Ryan Byrne <ryanbyrne142@yahoo.com>
2019-03-13 20:04:46 -0400

    fix bug in update_train_info

--- generate_stops.py
+++ generate_stops.py
@@ -9,21 +9,21 @@ class Generate():
     def update_train_info():
         current_trains = []
         id_array = ["1", "26", "16", "21", "2", "31", "36", "51"]
-        exclude = ["9", "S"]
         key = "aec131a835263208ee94b402590bb930"
+        exclude = ["9", "S"]
         for id in id_array:
             feed = gtfs_realtime_pb2.FeedMessage()
             response = urllib2.urlopen('http://datamine.mta.info/mta_esi.php?key='+key+'&feed_id='+id, 'rb')
             feed.ParseFromString(response.read())
             for entity in feed.entity:
-                for stop in entity.trip_update.stop_time_update:
-                    s = stop.stop_id[:-1]
-                    r = stop.stop_id[0]
-                    if s == "R60" or s in current_trains or r in exclude:
-                        continue
-                    else:
-                        current_trains.append(stop.stop_id[:-1])
-                    break
+                try:
+                    s = entity.trip_update.stop_time_update[0].stop_id[:-1]
+                except IndexError:
+                    continue
+                if s in current_trains:
+                    continue
+                else:
+                    current_trains.append(s)
         return current_trains
 
     # Generate the list of Station IDs currently with trains
